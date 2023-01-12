8fd914c174706954ef3d86a2d022ea6afb6dbceb
Dariussrz83 <120649002+Dariussrz83@users.noreply.github.com>
2023-01-12 10:08:19 -0300

    herencia

--- Aurora/urls.py
+++ Aurora/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from cakes.views import index
+
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cakes',include('cakes.urls')),
+    path('cakes/',include('cakes.urls')),
+    path('',index,name='index'),
    
 ]
